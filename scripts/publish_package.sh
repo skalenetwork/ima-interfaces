1761d94ef2fd6bfeadf987bea8f6df35373151f8
Dmytro Stebaiev <dmytro@skalelabs.com>
2021-10-26 14:56:04 +0300

    SKALE-4634 Setup package publishing

---
+++ scripts/publish_package.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+set -e
+
+USAGE_MSG='Usage: BRANCH=[BRANCH] publish_package.sh'
+if [ -z "$BRANCH" ]
+then
+    (>&2 echo 'You should provide branch')
+    echo "$USAGE_MSG"
+    exit 1
+fi
+
+cd "$(dirname "$0")/.."
+
+BRANCH=$(echo $BRANCH | tr [:upper:] [:lower:] | tr -d [:space:])
+VERSION=$(BRANCH=$BRANCH ./scripts/calculate_version.sh)
+
+TAG=""
+if ! [[ $BRANCH == 'stable' ]]
+then
+    TAG="--tag $BRANCH"
+fi
+
+yarn publish contracts --access public --new-version $VERSION --verbose --no-git-tag-version $TAG
