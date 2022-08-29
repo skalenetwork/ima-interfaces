e57284c93e6011330604550d7ec4141e64a2045b
Artem Payvin <payvinartem@gmail.com>
2022-08-29 18:25:23 +0100

    Add functions

--- contracts/mainnet/IMessageProxyForMainnet.sol
+++ contracts/mainnet/IMessageProxyForMainnet.sol
@@ -30,6 +30,9 @@ interface IMessageProxyForMainnet is IMessageProxy {
     function setNewMessageGasCost(uint256 newMessageGasCost) external;
     function pause(string calldata schainName) external;
     function resume(string calldata schainName) external;
+    function addReimbursedContract(string memory schainName, address reimbursedContract) external;
+    function removeExtraContract(string memory schainName, address reimbursedContract) external;
     function messageInProgress() external view returns (bool);
     function isPaused(bytes32 schainHash) external view returns (bool);
+    function isReimbursingContract(bytes32 schainHash, address contractAddress) external view returns (bool);
 }
